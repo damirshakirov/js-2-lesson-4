caaa9ee7f2aed8fe0eab409b8b0d4f551e9f18d2
damirTV <31268720+damirTV@users.noreply.github.com>
2018-05-20 23:01:52 +0300

    task1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-$(".task1__li").click(function () {
-  $('.task1__li').removeClass('active');
-  $(this).addClass('active');
-  $('.task1_p').removeClass('pActive');
-  switch (this.innerHTML) {
-    case 'Первая вкладка':
-      $('.1').addClass('pActive');
-      break;
-    case 'Вторая вкладка':
-      $('.2').addClass('pActive');
-      break;
-    case 'Третья вкладка':
-      $('.3').addClass('pActive');
-      break;
-    case 'Четвертая вкладка':
-      $('.4').addClass('pActive');
-      break;
-  }
-
+$(function() {
+  $(".task1__li").click(function () {
+    $('.task1__li').removeClass('active');
+    $(this).addClass('active');
+    $('.task1_p').removeClass('pActive');
+    switch (this.innerHTML) {
+      case 'Первая вкладка':
+        $('.1').addClass('pActive');
+        break;
+      case 'Вторая вкладка':
+        $('.2').addClass('pActive');
+        break;
+      case 'Третья вкладка':
+        $('.3').addClass('pActive');
+        break;
+      case 'Четвертая вкладка':
+        $('.4').addClass('pActive');
+        break;
+    }
+  });
 });
