dc4a2b9f969f61741bd6128f83b88ce1a7e5fe5c
damirTV <31268720+damirTV@users.noreply.github.com>
2018-05-21 15:33:35 +0300

    выгрузка на гб

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$(function() {
+// Задание №1
+$(function () {
   $(".task1__li").click(function () {
     $('.task1__li').removeClass('active');
     $(this).addClass('active');
@@ -18,4 +19,53 @@ $(function() {
         break;
     }
   });
+});
+
+// Задание №2
+check = function (form) {
+  let name = form.name.value,
+    phone = form.phone.value,
+    email = form.email.value,
+    text = form.text.value,
+    namePattern = /^[а-яА-ЯёЁa-zA-Z]+$/,
+    phonePattern = /^\+7\([0-9]{3}\)[0-9]{3}\-[0-9]{4}$/,
+    emailPattern = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    textPattern = /^.*$/,
+    errorText = '';
+  if (namePattern.test(name)) document.getElementById('name').style.border = '1px solid green';
+  else {
+    document.getElementById('name').style.border = '1px solid red';
+    errorText += 'Имя должно содержать только буквы\n';
+  }
+  if (phonePattern.test(phone)) document.getElementById('phone').style.border = '1px solid green';
+  else {
+    document.getElementById('phone').style.border = '1px solid red';
+    errorText += 'Телефон должен быть в формате +7(000)000-0000\n';
+  }
+  if (emailPattern.test(email)) document.getElementById('email').style.border = '1px solid green';
+  else {
+    document.getElementById('email').style.border = '1px solid red';
+    errorText += 'Укажите корректный email\n';
+  }
+  if (textPattern.test(text)) document.getElementById('text').style.border = '1px solid green';
+  else {
+    document.getElementById('text').style.border = '1px solid red';
+    errorText += 'Заполните поле текст произвольным текстом';
+  }
+  if (errorText != '') alert(errorText);
+  else alert('Данные отправлены. Спасибо!');
+}
+
+//Предективный поиск города
+
+$(function () { //Загрузка JSON файла
+  $.getJSON("js/cities.json", function (json) {
+    console.log(json);
+  });
+
+
+  $('#city').keyup(function () { // Проверка ввода с клавиатуры
+    let city = ($('#city').val());
+    console.log(city);
+  })
 });
